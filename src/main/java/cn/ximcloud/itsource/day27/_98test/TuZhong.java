17599f5fe46e3aae5ddd1b8e882195fb219dea41
usamimizugi <715759898@qq.com>
2018-07-16 16:20:05 +0800

    day27 自习

--- src/main/java/cn/ximcloud/itsource/day27/_98test/TuZhong.java
+++ src/main/java/cn/ximcloud/itsource/day27/_98test/TuZhong.java
@@ -1,11 +1,16 @@
-package cn.ximcloud.itsource.day27.homework;
+package cn.ximcloud.itsource.day27._98test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
  * User: wzard
  * Date: 2018-07-16
- * Time: 14:42
- * ProjectName: ITSource.cn.ximcloud.itsource.day27.homework
+ * Time: 16:15
+ * ProjectName: ITSource.cn.ximcloud.itsource.day27._98test
  * To change this template use File | Settings | Editor | File and Code Templates.
  * ////////////////////////////////////////////////////////////////////
  * //                          _ooOoo_                               //
@@ -31,5 +36,28 @@ package cn.ximcloud.itsource.day27.homework;
  * ////////////////////////////////////////////////////////////////////
  **/
 
-public class HomeWork2 {
+public class TuZhong{
+    public static void main(String[] args) {
+        String mulu = "C:\\Users\\wizard\\Desktop\\";   //文件夹
+        String pic = "ohayou.jpg";    //图片
+        String rar = "tu.zip";    //压缩包
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(mulu + rar));
+            FileInputStream fileInputStream1 = new FileInputStream(new File(mulu + pic));
+            FileOutputStream fileOutputStream = new FileOutputStream(mulu + rar+".jpg");
+            int i;
+            byte[] bytes = new byte[1024];
+            while ((i = fileInputStream1.read(bytes)) != -1) {
+                fileOutputStream.write(bytes, 0, i);
+            }
+            while ((i = fileInputStream.read(bytes)) != -1) {
+                fileOutputStream.write(bytes, 0, i);
+            }
+            fileInputStream.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+

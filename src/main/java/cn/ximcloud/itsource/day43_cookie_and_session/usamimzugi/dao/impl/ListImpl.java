99ebc133baa814309d2ce5c63cd7c51bd86d521c
Wizard <715759898@qq.com>
2018-08-11 00:39:27 +0800

    DAY43 release

---
+++ src/main/java/cn/ximcloud/itsource/day43_cookie_and_session/usamimzugi/dao/impl/ListImpl.java
@@ -0,0 +1,90 @@
+package cn.ximcloud.itsource.day43_cookie_and_session.usamimzugi.dao.impl;
+
+import cn.ximcloud.itsource.day43_cookie_and_session.usamimzugi.dao.IBaseDao;
+import cn.ximcloud.itsource.day43_cookie_and_session.usamimzugi.dao.IListDao;
+import cn.ximcloud.itsource.day43_cookie_and_session.usamimzugi.domain.List;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Wizard
+ * Date: 2018-08-10
+ * Time: 23:58
+ * ProjectName: itsource
+ * To change this template use File | Settings | File Templates.
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //         佛祖保佑          永无BUG          永不修改             //
+ * ////////////////////////////////////////////////////////////////////
+ **/
+public class ListImpl extends BaseImpl<List> implements IListDao<List> {
+    /**
+     * 保存方法，保存一个T
+     *
+     * @param list 一个实例对象
+     */
+    @Override
+    public void save(List list) {
+
+    }
+
+    /**
+     * 更新方法，更新该对象
+     *
+     * @param list 通过传入一个对象来查询并更新
+     */
+    @Override
+    public void update(List list) {
+
+    }
+
+    /**
+     * 删除一个对象
+     *
+     * @param id 通过id主键来查找实例，然后删除该实例
+     */
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    /**
+     * 查找用户
+     *
+     * @param id 通过id主键来查找实力
+     * @return 返回一个用户实例，如果没有查找到实例，返回null
+     */
+    @Override
+    public List find(Integer id) {
+        return null;
+    }
+
+    /**
+     * 查找所有的用户
+     *
+     * @return 返回一个ArrayList表，表里面存放的是每一个实例
+     */
+    @Override
+    public ArrayList<List> findAll() {
+        return null;
+    }
+}
---
+++ src/main/java/cn/ximcloud/itsource/day43_cookie_and_session/usamimzugi/servlet/InitServlet.java
@@ -0,0 +1,69 @@
+package cn.ximcloud.itsource.day43_cookie_and_session.usamimzugi.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Wizard
+ * Date: 2018-08-11
+ * Time: 00:03
+ * ProjectName: itsource
+ * To change this template use File | Settings | File Templates.
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //         佛祖保佑          永无BUG          永不修改             //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @author Wizard*/
+@WebServlet(loadOnStartup = 1)
+public class InitServlet implements Servlet {
+    /**
+     * 这个实例仅仅是用于做初始化操作的
+     * @param servletConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

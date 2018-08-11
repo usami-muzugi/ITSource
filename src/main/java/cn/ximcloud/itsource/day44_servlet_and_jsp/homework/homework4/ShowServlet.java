94db8bff8929a0a0e27823b985cdb0a66998ca2c
usamimizugi <715759898@qq.com>
2018-08-12 02:04:19 +0800

    day44 release

---
+++ src/main/java/cn/ximcloud/itsource/day44_servlet_and_jsp/homework/homework4/ShowServlet.java
@@ -0,0 +1,61 @@
+package cn.ximcloud.itsource.day44_servlet_and_jsp.homework.homework4;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: wzard
+ * @date: 2018-08-12
+ * Time: 01:55
+ * ProjectName: itsource.cn.ximcloud.itsource.day44_servlet_and_jsp.homework.homework4
+ * To change this template use File | Settings | Editor | File and Code Templates.
+ * <p>
+ * you are not expected to understand this.
+ * <p>
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //         佛祖保佑          永无BUG     永不修改                  //
+ * ////////////////////////////////////////////////////////////////////
+ **/
+@WebServlet(name = "day44_ShowServlet", urlPatterns = "/day44/show")
+public class ShowServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        servletContext.setAttribute("","");
+
+        HttpSession session = req.getSession();
+        session.setAttribute("", "");
+
+        req.setAttribute("", "");
+
+
+    }
+}
